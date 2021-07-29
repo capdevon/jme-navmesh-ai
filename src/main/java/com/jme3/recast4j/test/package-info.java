a430cbf7ad8565ac3e64c51e583cb4d39db2fbf9
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-07-29 09:42:53 +0200

    Delete package-info.java

--- src/main/java/com/jme3/recast4j/test/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package handles scenarios used in jme3-recast4j-demo to easily check out special conditions as well as to
- * provide a good set of templates for Unit-Testing.
- * It is not meant intended to be used by user-code and as such API could break anytime.
- *
- */
-package com.jme3.recast4j.test;
