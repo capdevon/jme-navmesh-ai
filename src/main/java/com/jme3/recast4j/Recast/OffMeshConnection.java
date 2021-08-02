ea83b6de657da0b2885b46fbe0abcacbe7e2782c
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-08-02 10:28:34 +0200

    removed jme3 objects (Vector3f)

--- src/main/java/com/jme3/recast4j/Recast/OffMeshConnection.java
+++ src/main/java/com/jme3/recast4j/Recast/OffMeshConnection.java
@@ -1,107 +1,50 @@
-/*
- *  MIT License
- *  Copyright (c) 2018 MeFisto94
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- */
 package com.jme3.recast4j.Recast;
 
-import com.jme3.math.Vector3f;
+import org.recast4j.detour.NavMesh;
 
+/**
+ * 
+ * @author capdevon
+ */
 public class OffMeshConnection {
 
-    /** Off-mesh connection vertices. [Unit: wu] **/
-    Vector3f A;
-    /** Off-mesh connection vertices. [Unit: wu] **/
-    Vector3f B;
-    /** Off-mesh connection radii. [Unit: wu] **/
-    float radius;
-    /** User defined flags assigned to the off-mesh connections. **/
-    int flags;
+	// Off-mesh connection vertices.
+	public final float[] verts;
+    // Off-mesh connection radii.
+    public final float radius;
+    // User defined flags assigned to the off-mesh connections.
+    public final int flags;
 
-    /** User defined area ids assigned to the off-mesh connections.
+    /** 
+     * User defined area ids assigned to the off-mesh connections.
      * (https://digestingduck.blogspot.com/2010/01/off-mesh-connection-progress-pt-3.html?showComment=1334596410261#c3440987512618011308)
      * Can be used to weight off-mesh-connections based on their area type (teleporter would be a different area than elevator)
      */
-    int areas;
+    public final int areas;
 
-    /** The permitted travel direction of the off-mesh connections.
-     *  See {@link OffMeshConnectionDirection} for more information
+    /** 
+     * The permitted travel direction of the off-mesh connections.
+     *  0 = Travel only from endpoint A to endpoint B
+     *  #NavMesh.DT_OFFMESH_CON_BIDIR = Bidirectional travel.
      */
-    OffMeshConnectionDirection direction;
+    public final int direction;
 
     /** User-Defined ID, could be used to identify this in your game world together with {@link #areas} */
-    int userId;
-
-    public Vector3f getA() {
-        return A;
-    }
-
-    public void setA(Vector3f a) {
-        A = a;
-    }
-
-    public Vector3f getB() {
-        return B;
-    }
-
-    public void setB(Vector3f b) {
-        B = b;
-    }
-
-    public float getRadius() {
-        return radius;
-    }
-
-    public void setRadius(float radius) {
-        this.radius = radius;
-    }
-
-    public int getFlags() {
-        return flags;
-    }
-
-    public void setFlags(int flags) {
-        this.flags = flags;
-    }
-
-    public int getAreas() {
-        return areas;
-    }
-
-    public void setAreas(int areas) {
-        this.areas = areas;
-    }
-
-    public OffMeshConnectionDirection getDirection() {
-        return direction;
-    }
-
-    public void setDirection(OffMeshConnectionDirection direction) {
-        this.direction = direction;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
+    public final int userID;
+    
+	public OffMeshConnection(float[] start, float[] end, float radius, boolean bidir, int areas, int flags, int userID) {
+		verts = new float[6];
+        verts[0] = start[0];
+        verts[1] = start[1];
+        verts[2] = start[2];
+        verts[3] = end[0];
+        verts[4] = end[1];
+        verts[5] = end[2];
+		this.radius = radius;
+		this.flags = flags;
+		this.areas = areas;
+		this.direction = bidir ? NavMesh.DT_OFFMESH_CON_BIDIR : 0;
+		this.userID = userID;
+	}
+    
 }
