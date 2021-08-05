c55fae9f0a6d8861fa3ba0976725b8b9182ed6fd
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-08-05 10:19:12 +0200

    Update OffMeshConnection.java

--- src/main/java/com/jme3/recast4j/Recast/OffMeshConnection.java
+++ src/main/java/com/jme3/recast4j/Recast/OffMeshConnection.java
@@ -3,13 +3,16 @@ package com.jme3.recast4j.Recast;
 import org.recast4j.detour.NavMesh;
 
 /**
+ * Defines a navigation mesh off-mesh connection within a dtMeshTile object. An
+ * off-mesh connection is a user defined traversable connection made up to two
+ * vertices.
  * 
  * @author capdevon
  */
-public class OffMeshConnection {
+public class OffMeshLink {
 
-	// Off-mesh connection vertices.
-	public final float[] verts;
+    // Off-mesh connection vertices.
+    public final float[] verts;
     // Off-mesh connection radii.
     public final float radius;
     // User defined flags assigned to the off-mesh connections.
@@ -24,27 +27,27 @@ public class OffMeshConnection {
 
     /** 
      * The permitted travel direction of the off-mesh connections.
-     *  0 = Travel only from endpoint A to endpoint B
-     *  #NavMesh.DT_OFFMESH_CON_BIDIR = Bidirectional travel.
+     * 0 = Travel only from endpoint A to endpoint B
+     * #NavMesh.DT_OFFMESH_CON_BIDIR = Bidirectional travel.
      */
     public final int direction;
 
     /** User-Defined ID, could be used to identify this in your game world together with {@link #areas} */
     public final int userID;
-    
-	public OffMeshConnection(float[] start, float[] end, float radius, boolean bidir, int areas, int flags, int userID) {
-		verts = new float[6];
+
+    public OffMeshLink(float[] start, float[] end, float radius, boolean biDirectional, int areas, int flags, int userID) {
+        verts = new float[6];
         verts[0] = start[0];
         verts[1] = start[1];
         verts[2] = start[2];
         verts[3] = end[0];
         verts[4] = end[1];
         verts[5] = end[2];
-		this.radius = radius;
-		this.flags = flags;
-		this.areas = areas;
-		this.direction = bidir ? NavMesh.DT_OFFMESH_CON_BIDIR : 0;
-		this.userID = userID;
-	}
-    
+        this.radius = radius;
+        this.flags = flags;
+        this.areas = areas;
+        this.direction = biDirectional ? NavMesh.DT_OFFMESH_CON_BIDIR : 0;
+        this.userID = userID;
+    }
+
 }
