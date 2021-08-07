a74f67e0e7b262afc7be7a28bfd0f9b7c0c5f80e
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-08-07 21:25:27 +0200

    NEW Control

--- src/main/java/mygame/controls/PCControl.java
+++ src/main/java/mygame/controls/PCControl.java
@@ -1,119 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package mygame.controls;
-import com.jme3.bullet.collision.shapes.CollisionShape;
-import com.jme3.bullet.collision.shapes.CompoundCollisionShape;
-import com.jme3.bullet.collision.shapes.CylinderCollisionShape;
-import com.jme3.bullet.collision.shapes.SphereCollisionShape;
-import com.jme3.bullet.control.BetterCharacterControl;
-import com.jme3.input.controls.ActionListener;
-import com.jme3.math.Vector3f;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.AnimEventListener;
+import com.jme3.recast4j.ai.NavMeshAgent;
 import com.jme3.scene.Spatial;
 
-import mygame.interfaces.DataKey;
-import mygame.interfaces.EnumPosType;
-import mygame.interfaces.ListenerKey;
-
 /**
- * Controls the spatials movement. Speed is derived from EnumPosType.
- * 
- * @author mitm
+ *
+ * @author capdevon
  */
-public class PCControl extends BetterCharacterControl implements ActionListener {
+public class PCControl extends AdapterControl implements AnimEventListener {
 
-    private boolean forward;
-    private float moveSpeed;
-    private int position;
-
-    public PCControl(float radius, float height, float mass) {
-        super(radius, height, mass);
-    }
+    private NavMeshAgent agent;
+    private AnimationControl animator;
 
     @Override
-    public void update(float tpf) {
-        super.update(tpf);
-        this.moveSpeed = 0;
-        Vector3f modelForwardDir = spatial.getWorldRotation().mult(Vector3f.UNIT_Z);  
-        walkDirection.set(0, 0, 0);
-        if (forward) {
-            position = getPositionType();
-            for (EnumPosType pos : EnumPosType.values()) {
-                if (pos.pos() == position) {
-                    switch (pos) {
-                        case POS_SWIMMING:
-                            moveSpeed = EnumPosType.POS_SWIMMING.speed();
-                            break;
-                        case POS_WALKING:
-                            moveSpeed = EnumPosType.POS_WALKING.speed();
-                            break;
-                        case POS_RUNNING:
-                            moveSpeed = EnumPosType.POS_RUNNING.speed();
-                            break;
-                        default:
-                            moveSpeed = 0f;
-                            break;
-                    }
-                }
-            }
-//            if (this.rigidBody.getLinearVelocity().length() > this.getMoveSpeed()) {
-//                System.out.println("Velocity = " + this.rigidBody.getLinearVelocity().length());
-//            }
-            walkDirection.addLocal(modelForwardDir.mult(moveSpeed));
-        }
-        setWalkDirection(walkDirection);
-    }
-
-    @Override
-    public void onAction(String name, boolean isPressed, float tpf) {
-        if (name.equals(ListenerKey.MOVE_FORWARD)) {
-            forward = isPressed;
-        }
-        if (name.equals((ListenerKey.JUMP))) {
-            jump();
+    public void setSpatial(Spatial spatial) {
+        super.setSpatial(spatial);
+        if (spatial != null) {
+            this.agent = getComponent(NavMeshAgent.class);
+            requireNonNull(agent, NavMeshAgent.class, PCControl.class);
+            
+            this.animator = getComponent(AnimationControl.class);
+            requireNonNull(animator, AnimationControl.class, PCControl.class);
+            
+            animator.addAnimListener(this);
         }
     }
 
-    //Override default collisionshape due to .7 offset.
     @Override
-    protected CollisionShape getShape() {
-        float radius = getFinalRadius();
-        float height = getFinalHeight();
-        float cylinder_height = height - (2.0f * radius);
-        CylinderCollisionShape cylinder = new CylinderCollisionShape(
-                new Vector3f(radius, cylinder_height / 2f, radius)/*NB constructor want half extents*/, 1);
-        SphereCollisionShape sphere = new SphereCollisionShape(getFinalRadius());
-        CompoundCollisionShape compoundCollisionShape = new CompoundCollisionShape();
-        compoundCollisionShape.addChildShape(sphere,
-                new Vector3f(0,/*sphere half height*/ radius, 0)); // bottom sphere
-        compoundCollisionShape.addChildShape(cylinder,
-                new Vector3f(0,/*half sphere height*/ (radius) +/*cylinder half height*/ (cylinder_height / 2.f), 0)); // cylinder, on top of the bottom sphere
-        compoundCollisionShape.addChildShape(sphere,
-                new Vector3f(0,/*half sphere height*/ (radius) +/*cylinder height*/ (cylinder_height), 0)); // top sphere       
-        return compoundCollisionShape;
-    }
-    
-    //need to overide because we extended BetterCharacterControl
-    @Override
-    public PCControl cloneForSpatial(Spatial spatial) {
-        try {
-            PCControl control = (PCControl) super.clone();
-            control.setSpatial(spatial); 
-            return control;
-        } catch (CloneNotSupportedException ex) {
-            throw new RuntimeException("Clone Not Supported", ex);
+    public void controlUpdate(float tpf) {
+        if (!agent.pathPending() && agent.isAtGoal()) {
+            animator.setAnimation("Idle");
+        } else {
+            animator.setAnimation("Walk");
         }
     }
 
-    //need to override because we extended BetterCharacterControl
-    @Override
-    public PCControl jmeClone() {
-        try {
-            return (PCControl) super.clone();
-        } catch (CloneNotSupportedException ex) {
-            throw new RuntimeException("Clone Not Supported", ex);
-        }
-    }
-    
-    //gets the physical pos of spatial
-    private int getPositionType() {
-        return (int) spatial.getUserData(DataKey.POSITION_TYPE);
-    }
+	@Override
+	public void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName) {
+	}
+
+	@Override
+	public void onAnimChange(AnimControl control, AnimChannel channel, String animName) {
+		System.out.println("onAnimChange: " + animName);
+	}
+
 }
