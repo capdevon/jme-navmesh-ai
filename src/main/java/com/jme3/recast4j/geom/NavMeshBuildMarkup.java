272de22f53aa83c45a3fabc8fd324db63766f2ab
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-08-15 20:46:43 +0200

    format code

--- src/main/java/com/jme3/recast4j/geom/NavMeshBuildMarkup.java
+++ src/main/java/com/jme3/recast4j/geom/NavMeshBuildMarkup.java
@@ -17,39 +17,39 @@ import com.jme3.scene.Spatial;
  */
 public class NavMeshBuildMarkup {
 
-	// Use this to specify which GameObject (including the GameObject’s children)
-	// the markup should be applied to.
-	public final Spatial root;
-	// Use this to specify whether the GameObject and its children should be ignored.
-	public final boolean ignoreFromBuild;
-	// Use this to specify whether the area type of the GameObject and its children
-	// should be overridden by the area type specified in this struct.
-	public final boolean overrideArea;
-	// The area type to use when override area is enabled.
-	public final AreaModification area;
+    // Use this to specify which GameObject (including the GameObjectâ€™s children)
+    // the markup should be applied to.
+    public final Spatial root;
+    // Use this to specify whether the GameObject and its children should be ignored.
+    public final boolean ignoreFromBuild;
+    // Use this to specify whether the area type of the GameObject and its children
+    // should be overridden by the area type specified in this struct.
+    public final boolean overrideArea;
+    // The area type to use when override area is enabled.
+    public final AreaModification area;
 
-	/**
-	 * 
-	 * @param root
-	 * @param ignoreFromBuild
-	 */
-	public NavMeshBuildMarkup(Spatial root, boolean ignoreFromBuild) {
-		this.root = root;
-		this.ignoreFromBuild = ignoreFromBuild;
-		this.area = null;
-		this.overrideArea = false;
-	}
+    /**
+     * 
+     * @param root
+     * @param ignoreFromBuild
+     */
+    public NavMeshBuildMarkup(Spatial root, boolean ignoreFromBuild) {
+        this.root = root;
+        this.ignoreFromBuild = ignoreFromBuild;
+        this.area = null;
+        this.overrideArea = false;
+    }
 
-	/**
-	 * 
-	 * @param root
-	 * @param area
-	 */
-	public NavMeshBuildMarkup(Spatial root, AreaModification area) {
-		this.root = root;
-		this.area = area;
-		this.overrideArea = true;
-		this.ignoreFromBuild = false;
-	}
+    /**
+     * 
+     * @param root
+     * @param area
+     */
+    public NavMeshBuildMarkup(Spatial root, AreaModification area) {
+        this.root = root;
+        this.area = area;
+        this.overrideArea = true;
+        this.ignoreFromBuild = false;
+    }
 
 }
