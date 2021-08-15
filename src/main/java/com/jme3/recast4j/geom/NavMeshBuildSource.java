0e29b4e48452dc8e047e24cb5896c2280f4901ba
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-08-15 20:48:17 +0200

    format code

--- src/main/java/com/jme3/recast4j/geom/NavMeshBuildSource.java
+++ src/main/java/com/jme3/recast4j/geom/NavMeshBuildSource.java
@@ -13,41 +13,41 @@ import com.jme3.scene.Geometry;
  */
 public class NavMeshBuildSource {
 
-	// Geometry input sources.
-	public final Geometry sourceObj;
-	// Describes the area type of the NavMesh surface for this object.
-	public final AreaModification areaMod;
+    // Geometry input sources.
+    public final Geometry sourceObj;
+    // Describes the area type of the NavMesh surface for this object.
+    public final AreaModification area;
 
-	/**
-	 * 
-	 * @param sourceObj Geometry to modify.
-	 * @param areaMod   AreaModification to set.
-	 */
-	public NavMeshBuildSource(Geometry sourceObj, AreaModification areaMod) {
-		this.sourceObj = sourceObj;
-		this.areaMod = areaMod;
-	}
+    /**
+     * 
+     * @param sourceObj Geometry to modify.
+     * @param areaMod   AreaModification to set.
+     */
+    public NavMeshBuildSource(Geometry sourceObj, AreaModification area) {
+        this.sourceObj = sourceObj;
+        this.area = area;
+    }
 
-	/**
-	 * @return the geomLength
-	 */
-	public int getGeomLength() {
-		return sourceObj.getMesh().getTriangleCount() * 3;
-	}
+    /**
+     * @return the geomLength
+     */
+    public int getGeomLength() {
+        return sourceObj.getMesh().getTriangleCount() * 3;
+    }
 
-	/**
-	 * @return the AreaModification.
-	 */
-	public AreaModification getAreaModification() {
-		return areaMod;
-	}
+    /**
+     * @return the AreaModification.
+     */
+    public AreaModification getAreaModification() {
+        return area;
+    }
 
-	@Override
-	public String toString() {
-		return "NavMeshBuildSource [sourceObj=" + sourceObj 
-				+ ", geomLength=" + getGeomLength() 
-				+ ", areaMod=" + areaMod.getMaskedValue() 
-				+ "]";
-	}
+    @Override
+    public String toString() {
+        return "NavMeshBuildSource [sourceObj=" + sourceObj +
+            ", geomLength=" + getGeomLength() +
+            ", area=" + area.getMaskedValue() +
+            "]";
+    }
 
 }
