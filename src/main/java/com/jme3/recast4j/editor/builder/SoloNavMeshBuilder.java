c34622bd26052c51f2fd0dcc41111965e4d64660
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-08-24 18:25:38 +0200

    use JmeInputGeomProvider and JmeRecastBuilder

--- src/main/java/com/jme3/recast4j/editor/builder/SoloNavMeshBuilder.java
+++ src/main/java/com/jme3/recast4j/editor/builder/SoloNavMeshBuilder.java
@@ -4,13 +4,13 @@ import org.recast4j.detour.MeshData;
 import org.recast4j.detour.NavMesh;
 import org.recast4j.detour.NavMeshBuilder;
 import org.recast4j.detour.NavMeshDataCreateParams;
-import org.recast4j.recast.RecastBuilder;
+import org.recast4j.recast.RecastBuilder.RecastBuilderResult;
 import org.recast4j.recast.RecastBuilderConfig;
 import org.recast4j.recast.RecastConfig;
-import org.recast4j.recast.RecastBuilder.RecastBuilderResult;
-import org.recast4j.recast.geom.InputGeomProvider;
 
 import com.jme3.recast4j.editor.NavMeshBuildSettings;
+import com.jme3.recast4j.geom.JmeInputGeomProvider;
+import com.jme3.recast4j.geom.JmeRecastBuilder;
 
 /**
  * 
@@ -24,7 +24,7 @@ public class SoloNavMeshBuilder extends AbstractNavMeshBuilder {
      * @param s
      * @return
      */
-    public NavMesh build(InputGeomProvider m_geom, NavMeshBuildSettings s) {
+    public NavMesh build(JmeInputGeomProvider m_geom, NavMeshBuildSettings s) {
 
         // Initialize build config.
         RecastConfig cfg = new RecastConfig(s.partitionType, s.cellSize, s.cellHeight, s.agentHeight,
@@ -36,7 +36,7 @@ public class SoloNavMeshBuilder extends AbstractNavMeshBuilder {
         RecastBuilderConfig bcfg = new RecastBuilderConfig(cfg, m_geom.getMeshBoundsMin(), m_geom.getMeshBoundsMax());
 
         // Build our Navmesh data using our gathered geometry and configuration.
-        RecastBuilder rcBuilder = new RecastBuilder();
+        JmeRecastBuilder rcBuilder = new JmeRecastBuilder();
         RecastBuilderResult rcResult = rcBuilder.build(m_geom, bcfg);
 
         // Set the parameters needed to build our MeshData using the RecastBuilder results.
