049e591aad6130e36f6be7745a18ed963d0bdc0e
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-08-30 21:07:18 +0200

    Create StraightPathOptions.java

---
+++ src/main/java/com/jme3/recast4j/ai/StraightPathOptions.java
@@ -0,0 +1,28 @@
+package com.jme3.recast4j.ai;
+
+import org.recast4j.detour.NavMeshQuery;
+
+/**
+ * 
+ * @author capdevon
+ */
+public enum StraightPathOptions {
+
+    //None
+    None(0),
+    //Add a vertex at every polygon edge crossing where area changes.
+    AreaCrossings(NavMeshQuery.DT_STRAIGHTPATH_AREA_CROSSINGS),
+    //Add a vertex at every polygon edge crossing.
+    AllCrossings(NavMeshQuery.DT_STRAIGHTPATH_ALL_CROSSINGS);
+
+    private int value;
+
+    private StraightPathOptions(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
