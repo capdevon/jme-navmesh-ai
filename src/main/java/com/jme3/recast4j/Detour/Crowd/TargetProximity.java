1b6f3ee3c6324905f8544d5532232c159904b02f
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-09-03 18:20:13 +0200

    format code

--- src/main/java/com/jme3/recast4j/Detour/Crowd/TargetProximity.java
+++ src/main/java/com/jme3/recast4j/Detour/Crowd/TargetProximity.java
@@ -3,25 +3,29 @@ package com.jme3.recast4j.Detour.Crowd;
 import com.jme3.math.Vector3f;
 import org.recast4j.detour.crowd.CrowdAgent;
 
+/**
+ * 
+ * @author capdevon
+ */
 public class TargetProximity implements Proximity {
 
-	protected float distanceThreshold;
+    protected float distanceThreshold;
 
-	public TargetProximity(float distanceThreshold) {
-		setDistanceThreshold(distanceThreshold);
-	}
+    public TargetProximity(float distanceThreshold) {
+        setDistanceThreshold(distanceThreshold);
+    }
 
-	@Override
-	public boolean isInTargetProximity(CrowdAgent agent, Vector3f agentPos, Vector3f targetPos) {
-		return targetPos.subtract(agentPos).lengthSquared() < distanceThreshold * distanceThreshold;
-	}
+    @Override
+    public boolean isInTargetProximity(CrowdAgent agent, Vector3f agentPos, Vector3f targetPos) {
+        return targetPos.subtract(agentPos).lengthSquared() < distanceThreshold * distanceThreshold;
+    }
 
-	public float getDistanceThreshold() {
-		return distanceThreshold;
-	}
+    public float getDistanceThreshold() {
+        return distanceThreshold;
+    }
 
-	public void setDistanceThreshold(float distanceThreshold) {
-		this.distanceThreshold = distanceThreshold;
-	}
+    public void setDistanceThreshold(float distanceThreshold) {
+        this.distanceThreshold = distanceThreshold;
+    }
 
 }
