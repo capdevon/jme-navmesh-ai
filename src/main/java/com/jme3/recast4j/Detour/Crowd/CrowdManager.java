4924780c2058af3e5858f95f93667a2502f4f44f
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-09-03 18:10:45 +0200

    Complete rework of the Crowd package

--- src/main/java/com/jme3/recast4j/Detour/Crowd/CrowdManager.java
+++ src/main/java/com/jme3/recast4j/Detour/Crowd/CrowdManager.java
@@ -1,86 +1,110 @@
 package com.jme3.recast4j.Detour.Crowd;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
 
 public class CrowdManager {
-    CrowdUpdateType updateType;
-    ArrayList<Crowd> crowdList;
-    ReentrantLock lock;
+	
+	protected CrowdUpdateType updateType = CrowdUpdateType.SEQUENTIAL;
+	protected List<JmeCrowd> crowdList = new ArrayList<>();
+	protected ReentrantLock lock = new ReentrantLock();
 
-    public CrowdManager() {
-        updateType = CrowdUpdateType.SEQUENTIAL;
-        crowdList = new ArrayList<>();
-        lock = new ReentrantLock();
-    }
+	public CrowdUpdateType getCrowdUpdateType() {
+		return updateType;
+	}
+	
+	public void setUpdateType(CrowdUpdateType updateType) {
+		this.updateType = updateType;
+	}
 
-    public void setUpdateType(CrowdUpdateType updateType) {
-        this.updateType = updateType;
-    }
+	public void addCrowd(JmeCrowd c) {
+		try {
+			lock.lock();
+			crowdList.add(c);
+		} finally {
+			lock.unlock();
+		}
+	}
 
-    public void addCrowd(Crowd c) {
-        try {
-            lock.lock();
-            crowdList.add(c);
-        } finally {
-            lock.unlock();
-        }
-    }
+	public void removeCrowd(JmeCrowd c) {
+		try {
+			lock.lock();
+			crowdList.remove(c);
+		} finally {
+			lock.unlock();
+		}
+	}
 
-    public void removeCrowd(Crowd c) {
-        try {
-            lock.lock();
-            crowdList.remove(c);
-        } finally {
-            lock.unlock();
-        }
-    }
+	public JmeCrowd getCrowd(int idx) {
+		try {
+			lock.lock();
+			return crowdList.get(idx);
+		} finally {
+			lock.unlock();
+		}
+	}
 
-    public Crowd getCrowd(int idx) {
-        try {
-            lock.lock();
-            return crowdList.get(idx);
-        } finally {
-            lock.unlock();
-        }
-    }
+	public int getNumberOfCrowds() {
+		try {
+			lock.lock();
+			return crowdList.size();
+		} finally {
+			lock.unlock();
+		}
 
-    public int getNumberOfCrowds() {
-        try {
-            lock.lock();
-            return crowdList.size();
-        } finally {
-            lock.unlock();
-        }
+	}
 
-    }
+	public void update(float timePassed) {
+		try {
+			// Danger here is that someone is blocking the lock and never releasing,
+			// freezing a whole application
+			lock.lock();
+			Stream<JmeCrowd> stream;
 
-    public void update(float timePassed) {
-        try {
-            // Danger here is that someone is blocking the lock and never releasing, freezing a whole application
-            lock.lock();
-            Stream<Crowd> stream;
+			switch (updateType) {
+			case SEQUENTIAL:
+				stream = crowdList.stream();
+				break;
 
-            switch (updateType) {
-                case SEQUENTIAL:
-                    stream = crowdList.stream();
-                    break;
+			case PARALLEL:
+				stream = crowdList.parallelStream();
+				break;
 
-                case PARALLEL:
-                    stream = crowdList.parallelStream();
-                    break;
+			default:
+				throw new IllegalArgumentException("Unknown Update Type");
+			}
 
-                default:
-                    throw new IllegalArgumentException("Unknown Update Type");
-            }
+			crowdList.forEach(c -> c.preUpdate(timePassed));
+			stream.forEach(c -> c.update(timePassed));
+			crowdList.forEach(JmeCrowd::applyMovements);
+		} finally {
+			lock.unlock();
+		}
+	}
+	
+	/**
+	 * Determines the way individual crowds are updated while being attached to a
+	 * Crowd Manager. Note that PARALLEL isn't the magic solution because it only
+	 * makes sense with many active crowds and can't fix the fact that large crowds
+	 * are straining the performance
+	 */
+	public enum CrowdUpdateType {
+		/**
+		 * Update one Crowd after another, all in the caller's thread (most probably the
+		 * game logic thread). You could however also have your own UI/AI Update Thread
+		 * there.
+		 */
+		SEQUENTIAL,
 
-            crowdList.forEach(c -> c.preUpdate(timePassed));
-            stream.forEach(c -> c.update(timePassed));
-            crowdList.forEach(Crowd::applyMovements);
-        } finally {
-            lock.unlock();
-        }
-    }
+		/**
+		 * Update all Crowds in Parallel using the Java 8 Stream API (So the Thread
+		 * Count is guessed automatically based on available Hardware). Note that this
+		 * is not always the best solution and in most cases might perform worse than
+		 * {@link #SEQUENTIAL}
+		 */
+		PARALLEL
+	}
 
 }
