d25ada5c7c6a68d07da0cd32e17056c35e046ee6
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-09-03 18:19:03 +0200

    format code

--- src/main/java/com/jme3/recast4j/Detour/Crowd/JmeCrowdAgent.java
+++ src/main/java/com/jme3/recast4j/Detour/Crowd/JmeCrowdAgent.java
@@ -13,29 +13,29 @@ import com.jme3.recast4j.Detour.DetourUtils;
  */
 public class JmeCrowdAgent {
 
-	public static boolean isMoving(CrowdAgent ca) {
-		return ca.active && (ca.state == CrowdAgentState.DT_CROWDAGENT_STATE_OFFMESH
-				|| ca.state == CrowdAgentState.DT_CROWDAGENT_STATE_WALKING);
-	}
+    public static boolean isMoving(CrowdAgent ca) {
+        return ca.active && (ca.state == CrowdAgentState.DT_CROWDAGENT_STATE_OFFMESH ||
+            ca.state == CrowdAgentState.DT_CROWDAGENT_STATE_WALKING);
+    }
 
-	public static boolean hasNoInvalidTarget(CrowdAgent ca) {
-		return ca.active && ca.targetState != MoveRequestState.DT_CROWDAGENT_TARGET_FAILED;
-	}
+    public static boolean hasNoInvalidTarget(CrowdAgent ca) {
+        return ca.active && ca.targetState != MoveRequestState.DT_CROWDAGENT_TARGET_FAILED;
+    }
 
-	public static boolean hasValidTarget(CrowdAgent ca) {
-		return ca.active && ca.targetState == CrowdAgent.MoveRequestState.DT_CROWDAGENT_TARGET_VALID;
-	}
+    public static boolean hasValidTarget(CrowdAgent ca) {
+        return ca.active && ca.targetState == CrowdAgent.MoveRequestState.DT_CROWDAGENT_TARGET_VALID;
+    }
 
-	public static boolean hasNoTarget(CrowdAgent ca) {
-		return ca.active && ca.targetState == CrowdAgent.MoveRequestState.DT_CROWDAGENT_TARGET_NONE;
-	}
+    public static boolean hasNoTarget(CrowdAgent ca) {
+        return ca.active && ca.targetState == CrowdAgent.MoveRequestState.DT_CROWDAGENT_TARGET_NONE;
+    }
 
-	/**
-	 * Gets the position of this crowd agent as used in the simulation.
-	 * 
-	 * @return the crowd agent position
-	 */
-	public static Vector3f getPosition(CrowdAgent ca) {
-		return DetourUtils.toVector3f(ca.npos);
-	}
+    /**
+     * Gets the position of this crowd agent as used in the simulation.
+     * 
+     * @return the crowd agent position
+     */
+    public static Vector3f getPosition(CrowdAgent ca) {
+        return DetourUtils.toVector3f(ca.npos);
+    }
 }
