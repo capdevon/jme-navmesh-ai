c45443f268cb089e6493928c5cf20767aa649542
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-09-04 09:43:17 +0200

    format code

--- src/main/java/com/jme3/recast4j/demo/states/CrowdState.java
+++ src/main/java/com/jme3/recast4j/demo/states/CrowdState.java
@@ -27,10 +27,8 @@ import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.collision.CollisionResults;
 import com.jme3.input.InputManager;
-import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
 import com.jme3.material.Materials;
@@ -38,12 +36,12 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Ray;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
-import com.jme3.recast4j.Detour.Crowd.CrowdDebugControl;
 import com.jme3.recast4j.Detour.Crowd.CrowdManagerAppState;
 import com.jme3.recast4j.Detour.Crowd.JmeCrowd;
 import com.jme3.recast4j.Detour.Crowd.MovementType;
 import com.jme3.recast4j.debug.NavMeshDebugViewer;
 import com.jme3.recast4j.debug.NavPathDebugViewer;
+import com.jme3.recast4j.demo.controls.CrowdDebugControl;
 import com.jme3.recast4j.editor.NavMeshBuildSettings;
 import com.jme3.recast4j.editor.builder.TileNavMeshBuilder;
 import com.jme3.recast4j.geom.JmeGeomProviderBuilder;
@@ -64,70 +62,70 @@ import com.jme3.texture.Texture;
  * @author capdevon
  */
 public class CrowdState extends BaseAppState {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(CrowdState.class.getName());
 
-	private ViewPort viewPort;
+    private static final Logger LOG = LoggerFactory.getLogger(CrowdState.class.getName());
+
+    private ViewPort viewPort;
     private AssetManager assetManager;
     private InputManager inputManager;
-    
+
     private NavMeshDebugViewer nmDebugViewer;
     private NavPathDebugViewer pathViewer;
     private NavMesh navMesh;
     private JmeCrowd crowd;
-    
+
     private Node worldMap = new Node("worldMap");
-    
+
     @Override
     protected void initialize(Application app) {
-    	this.viewPort = app.getViewPort();
-    	this.assetManager = app.getAssetManager();
-    	this.inputManager = app.getInputManager();
-    	
-    	nmDebugViewer = new NavMeshDebugViewer(app.getAssetManager());
-    	pathViewer = new NavPathDebugViewer(app.getAssetManager());
-    	
-    	worldMap.attachChild(createFloor());
-    	getRootNode().attachChild(worldMap);
-    	
-    	buildTiled();
+        this.viewPort = app.getViewPort();
+        this.assetManager = app.getAssetManager();
+        this.inputManager = app.getInputManager();
+
+        nmDebugViewer = new NavMeshDebugViewer(app.getAssetManager());
+        pathViewer = new NavPathDebugViewer(app.getAssetManager());
+
+        worldMap.attachChild(createFloor());
+        getRootNode().attachChild(worldMap);
+
+        buildTiled();
         buildCrowd();
-        
+
         addAgent(new Vector3f(-5, 0, 0));
-		addAgent(new Vector3f(-4f, 0.0f, -1f));
-		addAgent(new Vector3f(-3, 0, 0));
-		
+        addAgent(new Vector3f(-4f, 0.0f, -1f));
+        addAgent(new Vector3f(-3, 0, 0));
+
         initKeys();
     }
-    
+
     private Node getRootNode() {
-    	return ((SimpleApplication) getApplication()).getRootNode();
+        return ((SimpleApplication) getApplication()).getRootNode();
     }
-    
+
     protected PhysicsSpace getPhysicsSpace() {
         return getState(BulletAppState.class).getPhysicsSpace();
     }
-    
+
     private void initKeys() {
         inputManager.addMapping("crowd pick", new MouseButtonTrigger(MouseInput.BUTTON_RIGHT));
-//        inputManager.addMapping("crowd pick", new KeyTrigger(KeyInput.KEY_LSHIFT));
+        //inputManager.addMapping("crowd pick", new KeyTrigger(KeyInput.KEY_LSHIFT));
         inputManager.addListener(actionListener, "crowd pick");
     }
-    
+
     private ActionListener actionListener = new ActionListener() {
         @Override
         public void onAction(String name, boolean keyPressed, float tpf) {
-        	if (name.equals("crowd pick") && !keyPressed) {
-        		Vector3f locOnMap = getLocationOnMap();
-        		if (locOnMap != null) {
-        			pathViewer.clearPath();
-            		pathViewer.putBox(ColorRGBA.Yellow, locOnMap);
-            		setTarget(locOnMap);
-        		}
-        	}
+            if (name.equals("crowd pick") && !keyPressed) {
+                Vector3f locOnMap = getLocationOnMap();
+                if (locOnMap != null) {
+                    pathViewer.clearPath();
+                    pathViewer.putBox(ColorRGBA.Yellow, locOnMap);
+                    setTarget(locOnMap);
+                }
+            }
         }
     };
-    
+
     /**
      * Returns the Location on the Map which is currently under the Cursor. For this
      * we use the Camera to project the point onto the near and far plane (because
@@ -157,117 +155,114 @@ public class CrowdState extends BaseAppState {
         Ray ray = new Ray(click3d, dir);
         return ray;
     }
-    
-	private Geometry createFloor() {
-		Box box = new Box(40f, .1f, 40f);
-		box.scaleTextureCoordinates(new Vector2f(10, 10)); //5x5
-		Geometry geo = new Geometry("Colored Box", box);
-		
-//		Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
-//		mat.setBoolean("UseMaterialColors", true);
-//		mat.setColor("Ambient", ColorRGBA.LightGray);
-//		mat.setColor("Diffuse", ColorRGBA.LightGray);
-		
-		Material mat = new Material(assetManager, Materials.LIGHTING);
+
+    private Geometry createFloor() {
+        Box box = new Box(40f, .1f, 40f);
+        box.scaleTextureCoordinates(new Vector2f(10, 10)); //5x5
+        Geometry geo = new Geometry("Colored Box", box);
+
+        //Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        //mat.setBoolean("UseMaterialColors", true);
+        //mat.setColor("Ambient", ColorRGBA.LightGray);
+        //mat.setColor("Diffuse", ColorRGBA.LightGray);
+
+        Material mat = new Material(assetManager, Materials.LIGHTING);
         Texture texture = assetManager.loadTexture("Textures/Level/default_grid.png");
         texture.setWrap(Texture.WrapMode.Repeat);
         mat.setTexture("DiffuseMap", texture);
-		
-		geo.setMaterial(mat);
-		
-		CollisionShape shape = CollisionShapeFactory.createMeshShape(geo);
+
+        geo.setMaterial(mat);
+
+        CollisionShape shape = CollisionShapeFactory.createMeshShape(geo);
         RigidBodyControl rgb = new RigidBodyControl(shape, 0);
         geo.addControl(rgb);
         getPhysicsSpace().add(rgb);
-        
-		return geo;
-	}
-    
+
+        return geo;
+    }
+
     private void buildTiled() {
-    	JmeInputGeomProvider m_geom = new JmeGeomProviderBuilder(worldMap).build();
-    	NavMeshBuildSettings s = new NavMeshBuildSettings();
-    	s.tiled = true;
-    	
-    	TileNavMeshBuilder builder = new TileNavMeshBuilder();
-    	
-    	navMesh = builder.build(m_geom, s);
-    	
-    	nmDebugViewer.drawMeshBounds(m_geom);
-    	nmDebugViewer.drawNavMesh(navMesh, true);
-    	
-    	//saveToFile(navMesh, "test.nm");
+        JmeInputGeomProvider m_geom = new JmeGeomProviderBuilder(worldMap).build();
+        NavMeshBuildSettings s = new NavMeshBuildSettings();
+        s.tiled = true;
+
+        TileNavMeshBuilder builder = new TileNavMeshBuilder();
+
+        navMesh = builder.build(m_geom, s);
+
+        nmDebugViewer.drawMeshBounds(m_geom);
+        nmDebugViewer.drawNavMesh(navMesh, true);
+
+        //saveToFile(navMesh, "test.nm");
     }
-    
-	private void saveToFile(NavMesh nm, String fileName) {
-		try {
-			MeshSetWriter msw = new MeshSetWriter();
-			File f = new File(fileName);
-			System.out.println("Saving NavMesh=" + f.getAbsolutePath());
-			msw.write(new FileOutputStream(f), nm, ByteOrder.BIG_ENDIAN, false);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-    
-	private void buildCrowd() {
-		// Start crowd.
-		crowd = new JmeCrowd(100, .3f, navMesh);
-		crowd.setMovementType(MovementType.BETTER_CHARACTER_CONTROL);
-		// Add to CrowdManager.
-		getState(CrowdManagerAppState.class).getCrowdManager().addCrowd(crowd);
 
-		// Setup local avoidance params to different qualities.
-		// Use mostly default settings, copy from dtCrowd.
-		ObstacleAvoidanceParams params = new ObstacleAvoidanceParams(crowd.getObstacleAvoidanceParams(0));
+    private void saveToFile(NavMesh nm, String fileName) {
+        try {
+            MeshSetWriter msw = new MeshSetWriter();
+            File f = new File(fileName);
+            System.out.println("Saving NavMesh=" + f.getAbsolutePath());
+            msw.write(new FileOutputStream(f), nm, ByteOrder.BIG_ENDIAN, false);
 
-		// Low (11)
-		params.velBias = 0.5f;
-		params.adaptiveDivs = 5;
-		params.adaptiveRings = 2;
-		params.adaptiveDepth = 1;
-		crowd.setObstacleAvoidanceParams(0, params);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-		// Medium (22)
-		params.velBias = 0.5f;
-		params.adaptiveDivs = 5;
-		params.adaptiveRings = 2;
-		params.adaptiveDepth = 2;
-		crowd.setObstacleAvoidanceParams(1, params);
+    private void buildCrowd() {
+        // Start crowd.
+        crowd = new JmeCrowd(100, .3f, navMesh);
+        crowd.setMovementType(MovementType.BETTER_CHARACTER_CONTROL);
+        // Add to CrowdManager.
+        getState(CrowdManagerAppState.class).getCrowdManager().addCrowd(crowd);
 
-		// Good (45)
-		params.velBias = 0.5f;
-		params.adaptiveDivs = 7;
-		params.adaptiveRings = 2;
-		params.adaptiveDepth = 3;
-		crowd.setObstacleAvoidanceParams(2, params);
+        // Setup local avoidance params to different qualities.
+        // Use mostly default settings, copy from dtCrowd.
+        ObstacleAvoidanceParams params = new ObstacleAvoidanceParams(crowd.getObstacleAvoidanceParams(0));
 
-		// High (66)
-		params.velBias = 0.5f;
-		params.adaptiveDivs = 7;
-		params.adaptiveRings = 3;
-		params.adaptiveDepth = 3;
-		crowd.setObstacleAvoidanceParams(3, params);
-	}
+        // Low (11)
+        params.velBias = 0.5f;
+        params.adaptiveDivs = 5;
+        params.adaptiveRings = 2;
+        params.adaptiveDepth = 1;
+        crowd.setObstacleAvoidanceParams(0, params);
 
-    @Override
-    protected void cleanup(Application app) {
+        // Medium (22)
+        params.velBias = 0.5f;
+        params.adaptiveDivs = 5;
+        params.adaptiveRings = 2;
+        params.adaptiveDepth = 2;
+        crowd.setObstacleAvoidanceParams(1, params);
+
+        // Good (45)
+        params.velBias = 0.5f;
+        params.adaptiveDivs = 7;
+        params.adaptiveRings = 2;
+        params.adaptiveDepth = 3;
+        crowd.setObstacleAvoidanceParams(2, params);
+
+        // High (66)
+        params.velBias = 0.5f;
+        params.adaptiveDivs = 7;
+        params.adaptiveRings = 3;
+        params.adaptiveDepth = 3;
+        crowd.setObstacleAvoidanceParams(3, params);
     }
 
     @Override
-	protected void onEnable() {
-	}
+    protected void cleanup(Application app) {}
 
     @Override
-    protected void onDisable() {
-    }
-    
+    protected void onEnable() {}
+
+    @Override
+    protected void onDisable() {}
+
     @Override
     public void render(RenderManager rm) {
         nmDebugViewer.show(rm, viewPort);
         pathViewer.show(rm, viewPort);
     }
-    
+
     /**
      * Set the target for the crowd.
      * 
@@ -276,7 +271,7 @@ public class CrowdState extends BaseAppState {
     public void setTarget(Vector3f target) {
         crowd.requestMoveToTarget(target);
     }
-    
+
     public float agentRadius = 0.3f;
     public float agentHeight = 1.7f;
     public boolean m_anticipateTurns;
@@ -284,23 +279,23 @@ public class CrowdState extends BaseAppState {
     public boolean m_optimizeTopo = true;
     public boolean m_obstacleAvoidance;
     public boolean m_separation = true;
-    
-	private void addAgent(Vector3f location) {
 
-		Node model = createModel(location);
+    private void addAgent(Vector3f location) {
 
-		CrowdAgentParams ap = getAgentParams(model);
-		// Add agent to the crowd.
-		CrowdAgent agent = crowd.createAgent(model.getWorldTranslation(), ap);
-		// Set the spatial for the agent.
-		//crowd.setSpatialForAgent(agent, model);
-		// Add the debug control and set its visual and verbose state.
-		CrowdDebugControl cwDebug = new CrowdDebugControl(crowd, agent, createTorus(agentHeight));
-		cwDebug.setVisual(true);
-		cwDebug.setVerbose(false);
-		model.addControl(cwDebug);
-	}
-    
+        Node model = createModel(location);
+
+        CrowdAgentParams ap = getAgentParams(model);
+        // Add agent to the crowd.
+        CrowdAgent agent = crowd.createAgent(model.getWorldTranslation(), ap);
+        // Set the spatial for the agent.
+        //crowd.setSpatialForAgent(agent, model);
+        // Add the debug control and set its visual and verbose state.
+        CrowdDebugControl cwDebug = new CrowdDebugControl(crowd, agent, createHalo(agentHeight));
+        cwDebug.setVisual(true);
+        cwDebug.setVerbose(false);
+        model.addControl(cwDebug);
+    }
+
     private CrowdAgentParams getAgentParams(Spatial model) {
         CrowdAgentParams ap = new CrowdAgentParams();
         ap.radius = agentRadius;
@@ -315,7 +310,7 @@ public class CrowdState extends BaseAppState {
         ap.userData = model;
         return ap;
     }
-    
+
     private int getUpdateFlags() {
         int updateFlags = 0;
         if (m_anticipateTurns) {
@@ -335,58 +330,58 @@ public class CrowdState extends BaseAppState {
         }
         return updateFlags;
     }
-    
+
     private Node createModel(Vector3f position) {
-    	
-    	//Load the spatial that will represent the agent.
+
+        //Load the spatial that will represent the agent.
         Node model = (Node) assetManager.loadModel("Models/Jaime/Jaime.j3o");
         model.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
         //Set translation prior to adding controls.
         model.setLocalTranslation(position);
         //Add agent to the scene.
         getRootNode().attachChild(model);
-        
+
         //If we have a physics Crowd we need a physics compatible control to apply
         //movement and direction to the spatial.
         model.addControl(new BetterCharacterControl(agentRadius, agentHeight, 20f));
         getPhysicsSpace().add(model);
-        
-//        RigidBodyControl rbc = createRigidBody(agentRadius, agentHeight);
-//        model.addControl(rbc);
-//        getPhysicsSpace().add(rbc);
-        
+
+        //RigidBodyControl rbc = createRigidBody(agentRadius, agentHeight);
+        //model.addControl(rbc);
+        //getPhysicsSpace().add(rbc);
+
         return model;
     }
-    
-	private RigidBodyControl createRigidBody(float radius, float height) {
-//		BetterCharacterControl bcc = new BetterCharacterControl(radius, height, 1f);
-//		CollisionShape collShape = bcc.getRigidBody().getCollisionShape()
-		
-		CapsuleCollisionShape capsule = new CapsuleCollisionShape(radius, (height - (2 * radius)));
+
+    private RigidBodyControl createRigidBody(float radius, float height) {
+        //BetterCharacterControl bcc = new BetterCharacterControl(radius, height, 1f);
+        //CollisionShape collShape = bcc.getRigidBody().getCollisionShape()
+
+        CapsuleCollisionShape capsule = new CapsuleCollisionShape(radius, (height - (2 * radius)));
         CompoundCollisionShape collShape = new CompoundCollisionShape();
         Vector3f position = new Vector3f(0, (height / 2f), 0);
         collShape.addChildShape(capsule, position);
-        
-		// Setup root motion physics control
-		RigidBodyControl rbc = new RigidBodyControl(collShape);
-		// Kinematic mode must be enabled so character is not influenced by physics
-		rbc.setKinematic(true);
-		// Apply spatial transform to the collision shape
-		rbc.setKinematicSpatial(true);
-		
-		return rbc;
-	}
-	
-	private Geometry createTorus(float yHeight) {
-		// Were going to use a debug move control so setup geometry for later use.
-		Sphere sphere = new Sphere(16, 16, 0.2f);
-		Geometry geo = new Geometry("halo", sphere);
-		Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-		geo.setMaterial(mat);
-		geo.setLocalTranslation(0, yHeight + 0.5f, 0);
-		geo.setShadowMode(RenderQueue.ShadowMode.Off);
 
-		return geo;
-	}
-    
+        // Setup root motion physics control
+        RigidBodyControl rbc = new RigidBodyControl(collShape);
+        // Kinematic mode must be enabled so character is not influenced by physics
+        rbc.setKinematic(true);
+        // Apply spatial transform to the collision shape
+        rbc.setKinematicSpatial(true);
+
+        return rbc;
+    }
+
+    private Geometry createHalo(float yHeight) {
+        // Were going to use a debug move control so setup geometry for later use.
+        Sphere sphere = new Sphere(16, 16, 0.2f);
+        Geometry geo = new Geometry("halo", sphere);
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        geo.setMaterial(mat);
+        geo.setLocalTranslation(0, yHeight + 0.5f, 0);
+        geo.setShadowMode(RenderQueue.ShadowMode.Off);
+
+        return geo;
+    }
+
 }
