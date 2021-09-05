925eb0ab51cc0528df20bee1a77975c9003146b7
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-09-05 20:06:45 +0200

    Update MovementType.java

--- src/main/java/com/jme3/recast4j/Detour/Crowd/MovementType.java
+++ src/main/java/com/jme3/recast4j/Detour/Crowd/MovementType.java
@@ -2,9 +2,11 @@ package com.jme3.recast4j.Detour.Crowd;
 
 /**
  * This class defines how the results of Detour Crowd calculation are applied to
- * the World. This can be moving the spatial directly, moving a
- * BetterCharacterControl or some custom code.
+ * the World.
  */
 public enum MovementType {
-	NONE, DIRECT, BETTER_CHARACTER_CONTROL, CUSTOM
+    NONE,
+    SPATIAL,
+    PHYSICS_CHARACTER,
+    CUSTOM
 }
