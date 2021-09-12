d73802344b4157091d02cdd72e1755b814cd0ec5
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-09-12 17:19:55 +0200

    removed Singleton

--- src/main/java/com/jme3/recast4j/Detour/Crowd/CrowdManagerAppState.java
+++ src/main/java/com/jme3/recast4j/Detour/Crowd/CrowdManagerAppState.java
@@ -5,21 +5,12 @@ import com.jme3.app.state.BaseAppState;
 import com.jme3.util.SafeArrayList;
 
 /**
- * CrowdManagerAppState provide
- * easier interfacing with the jMonkeyEngine.<br />
+ * CrowdManagerAppState provide easier interfacing with the jMonkeyEngine.<br />
  * Use this as starting point for your entity-based approaches.
  */
 public class CrowdManagerAppState extends BaseAppState {
 
     protected SafeArrayList<JmeCrowd> crowdList = new SafeArrayList<>(JmeCrowd.class, 5);
-    
-    private static class Singleton {
-        private static final CrowdManagerAppState INSTANCE = new CrowdManagerAppState();
-    }
-
-    public static CrowdManagerAppState getInstance() {
-        return Singleton.INSTANCE;
-    }
 
     @Override
     protected void initialize(Application application) {}
@@ -35,7 +26,7 @@ public class CrowdManagerAppState extends BaseAppState {
 
     @Override
     public void update(float tpf) {
-        for (JmeCrowd crowd: crowdList) {
+        for (JmeCrowd crowd : crowdList) {
             crowd.update(tpf);
         }
     }
