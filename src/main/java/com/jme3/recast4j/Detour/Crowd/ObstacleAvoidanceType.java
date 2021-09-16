6b80ca0d6894c533c4020cc14c00a1f145d1b8ed
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-09-16 19:10:11 +0200

    Add files via upload

---
+++ src/main/java/com/jme3/recast4j/Detour/Crowd/ObstacleAvoidanceType.java
@@ -0,0 +1,28 @@
+package com.jme3.recast4j.Detour.Crowd;
+
+/**
+ * Level of obstacle avoidance.
+ * 
+ * @author capdevon
+ */
+public enum ObstacleAvoidanceType {
+	
+    // Enable simple avoidance. Low performance impact.
+    LowQuality(0),
+    // Medium avoidance. Medium performance impact
+    MedQuality(1),
+    // Good avoidance. High performance impact
+    GoodQuality(2),
+    // Enable highest precision. Highest performance impact.
+    HighQuality(3);
+
+    private int id;
+
+    private ObstacleAvoidanceType(int value) {
+        this.id = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
