5a4fc9542fcbde407411b5a87d6c4a38080fa566
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-10-15 18:35:49 +0200

    better camera and lighting

--- src/main/java/com/jme3/recast4j/demo/Test_Crowd.java
+++ src/main/java/com/jme3/recast4j/demo/Test_Crowd.java
@@ -1,10 +1,6 @@
 package com.jme3.recast4j.demo;
 
-import com.jme3.app.ChaseCameraAppState;
-import com.jme3.app.DebugKeysAppState;
 import com.jme3.app.SimpleApplication;
-import com.jme3.app.StatsAppState;
-import com.jme3.audio.AudioListenerState;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
@@ -17,18 +13,16 @@ import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.material.Materials;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.FastMath;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
-import com.jme3.post.FilterPostProcessor;
-import com.jme3.post.filters.FXAAFilter;
 import com.jme3.recast4j.Detour.Crowd.CrowdManagerAppState;
 import com.jme3.recast4j.demo.states.CrowdState;
-import com.jme3.recast4j.demo.states.NavState;
+import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
-import com.jme3.shadow.DirectionalLightShadowFilter;
+import com.jme3.shadow.DirectionalLightShadowRenderer;
+import com.jme3.shadow.EdgeFilteringMode;
 import com.jme3.system.AppSettings;
 import com.jme3.texture.Texture;
 
@@ -39,15 +33,6 @@ import com.jme3.texture.Texture;
 public class Test_Crowd extends SimpleApplication {
 
     private BulletAppState bullet;
-
-    public Test_Crowd() {
-        super(new StatsAppState(),
-            new AudioListenerState(),
-            new DebugKeysAppState(),
-            new CrowdManagerAppState(),
-            new CrowdState()
-        );
-    }
 
     /**
      * 
@@ -68,14 +53,29 @@ public class Test_Crowd extends SimpleApplication {
     public void simpleInitApp() {
         initPhysics();
         setupScene();
-        setupLightsAndFilters();
-        setupCamera();
-        setupInputKeys();
+        addLighting();
+        configureInput();
+        configureCamera();
+        
+        stateManager.attach(new CrowdManagerAppState());
+        stateManager.attach(new CrowdState());
+    }
+    
+    /**
+     * Configure the Camera during startup.
+     */
+    private void configureCamera() {
+        flyCam.setMoveSpeed(20);
+        flyCam.setDragToRotate(true);
+        cam.setLocation(new Vector3f(0, 6, 12));
+        cam.lookAt(Vector3f.ZERO, Vector3f.UNIT_Y);
     }
 
+    /**
+     * Configure physics during startup.
+     */
     private void initPhysics() {
         bullet = new BulletAppState();
-        // Performance is better when threading in parallel
         bullet.setThreadingType(BulletAppState.ThreadingType.PARALLEL);
         stateManager.attach(bullet);
     }
@@ -85,13 +85,13 @@ public class Test_Crowd extends SimpleApplication {
         rootNode.attachChild(scene);
 
         Box box = new Box(40f, .1f, 40f);
-        box.scaleTextureCoordinates(new Vector2f(10, 10));
+        box.scaleTextureCoordinates(new Vector2f(12, 12));
         Geometry floor = new Geometry("Floor", box);
 
         Material mat = new Material(assetManager, Materials.LIGHTING);
-        Texture texture = assetManager.loadTexture("Textures/Level/default_grid.png");
-        texture.setWrap(Texture.WrapMode.Repeat);
-        mat.setTexture("DiffuseMap", texture);
+        Texture diffMap = assetManager.loadTexture("Textures/Ground/default_grid.png");
+        diffMap.setWrap(Texture.WrapMode.Repeat);
+        mat.setTexture("DiffuseMap", diffMap);
         floor.setMaterial(mat);
 
         CollisionShape shape = CollisionShapeFactory.createMeshShape(floor);
@@ -101,12 +101,18 @@ public class Test_Crowd extends SimpleApplication {
         bullet.getPhysicsSpace().add(rbc);
     }
 
-    private void setupLightsAndFilters() {
-        //Set the atmosphere of the world, lights, post processing.
-        viewPort.setBackgroundColor(new ColorRGBA(0.5f, 0.6f, 0.7f, 1.0f));
+    /**
+     * Add lighting and shadows.
+     */
+    private void addLighting() {
+        rootNode.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
 
-        DirectionalLight sun = new DirectionalLight();
-        sun.setDirection(new Vector3f(-0.2f, -1, -0.3f).normalizeLocal());
+        // Set the viewport's background color to light blue.
+        ColorRGBA skyColor = new ColorRGBA(0.1f, 0.2f, 0.4f, 1f);
+        viewPort.setBackgroundColor(skyColor);
+
+        Vector3f lightDir = new Vector3f(-7f, -3f, -5f).normalizeLocal();
+        DirectionalLight sun = new DirectionalLight(lightDir);
         sun.setName("sun");
         rootNode.addLight(sun);
 
@@ -115,37 +121,20 @@ public class Test_Crowd extends SimpleApplication {
         ambient.setName("ambient");
         rootNode.addLight(ambient);
 
-        DirectionalLightShadowFilter shadowFilter = new DirectionalLightShadowFilter(assetManager, 4096, 2);
-        shadowFilter.setLight(sun);
-        shadowFilter.setShadowIntensity(0.4f);
-        shadowFilter.setShadowZExtend(256);
-
-        FXAAFilter fxaa = new FXAAFilter();
-
-        FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
-        fpp.addFilter(shadowFilter);
-        fpp.addFilter(fxaa);
-        viewPort.addProcessor(fpp);
+        // Render shadows based on the directional light.
+        viewPort.clearProcessors();
+        DirectionalLightShadowRenderer dlsr = new DirectionalLightShadowRenderer(assetManager, 2_048, 3);
+        dlsr.setEdgeFilteringMode(EdgeFilteringMode.PCFPOISSON);
+        dlsr.setEdgesThickness(5);
+        dlsr.setLight(sun);
+        dlsr.setShadowIntensity(0.4f);
+        viewPort.addProcessor(dlsr);
     }
-
-    private void setupCamera() {
-        Node target = new Node("MainCamera");
-
-        ChaseCameraAppState chaseCam = new ChaseCameraAppState();
-        chaseCam.setTarget(target);
-        stateManager.attach(chaseCam);
-        chaseCam.setInvertHorizontalAxis(true);
-        chaseCam.setInvertVerticalAxis(true);
-        chaseCam.setZoomSpeed(0.5f);
-        chaseCam.setMinDistance(1);
-        chaseCam.setMaxDistance(20);
-        chaseCam.setDefaultDistance(chaseCam.getMaxDistance());
-        chaseCam.setMinVerticalRotation(-FastMath.HALF_PI);
-        chaseCam.setRotationSpeed(3);
-        chaseCam.setDefaultVerticalRotation(0.3f);
-    }
-
-    private void setupInputKeys() {
+    
+    /**
+     * Configure keyboard input during startup.
+     */
+    private void configureInput() {
     	
         inputManager.addMapping("TOGGLE_PHYSX_DEBUG", new KeyTrigger(KeyInput.KEY_0));
         inputManager.addListener(new ActionListener() {
