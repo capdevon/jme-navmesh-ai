ea61c055338574dd02e3112a64403041dfa03ba3
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-10-15 19:16:27 +0200

    improved debugging tool
    Added cylinder with minimal geometry to display radius, height and agent status

--- src/main/java/com/jme3/recast4j/demo/controls/CrowdDebugControl.java
+++ src/main/java/com/jme3/recast4j/demo/controls/CrowdDebugControl.java
@@ -5,74 +5,81 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.asset.DesktopAssetManager;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
+import com.jme3.recast4j.demo.utils.Circle;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
-import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.Spatial.CullHint;
 import com.jme3.scene.control.AbstractControl;
-import com.jme3.scene.shape.Sphere;
+import com.jme3.scene.shape.Line;
 
 /**
  * A debugging control that displays visual, verbose or both debug information
  * about an agents MoveRequestState inside the crowd.
  * 
- * @author Robert
  * @author capdevon
  */
 public class CrowdDebugControl extends AbstractControl {
 
-    private static final Logger LOG = LoggerFactory.getLogger(CrowdDebugControl.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(CrowdDebugControl.class.getName());
 
-    private final CrowdAgent agent;
-    private final Geometry halo;
+    private CrowdAgent agent;
+    private Node debugNode;
+    private Material wireMaterial;
     private ColorRGBA curColor;
-    private boolean visual;
-    private boolean verbose;
-    private float timer;
-    private float refreshTime = 1f;
+    private boolean visual = true;
+    private boolean verbose = false;
+    private float timer = 0;
+    private float refreshTime = 0.1f;
 
     /**
      * This control will display a visual, verbose, or both representation of an
      * agents MoveRequestState while inside the given crowd.
      * 
      * @param agent The agent to look for inside the crowd.
+     */
+    public CrowdDebugControl(CrowdAgent agent) {
+        this.agent = agent;
+        setupDebugNode(new DesktopAssetManager(true));
+    }
+
+    /**
+     * This control will display a visual, verbose, or both representation of an
+     * agents MoveRequestState while inside the given crowd.
+     * 
+     * @param agent        The agent to look for inside the crowd.
      * @param assetManager The AssetManager
      */
     public CrowdDebugControl(CrowdAgent agent, AssetManager assetManager) {
         this.agent = agent;
-        this.halo = createHalo(assetManager);
+        setupDebugNode(assetManager);
     }
 
-    /**
-     * A Geometry that will be used as the visual representation for the agents
-     * MoveRequestState.
-     * 
-     * @param assetManager
-     * @return
-     */
-    private Geometry createHalo(AssetManager assetManager) {
-        Sphere sphere = new Sphere(16, 16, 0.1f);
-        Geometry geo = new Geometry("halo", sphere);
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        geo.setMaterial(mat);
-        geo.setShadowMode(RenderQueue.ShadowMode.Off);
-        geo.setLocalTranslation(0, agent.params.height + 0.5f, 0);
+    private void setupDebugNode(AssetManager assetManager) {
+        wireMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        wireMaterial.getAdditionalRenderState().setWireframe(true);
+        wireMaterial.setColor("Color", ColorRGBA.White);
 
-        return geo;
+        float radius = agent.params.radius;
+        float height = agent.params.height;
+        debugNode = createDebugCylinder(radius, height);
     }
 
     @Override
     public void setSpatial(Spatial spatial) {
         super.setSpatial(spatial);
         if (spatial != null) {
-            ((Node) spatial).attachChild(halo);
+            ((Node) spatial).attachChild(debugNode);
         } else {
-            halo.removeFromParent();
+            debugNode.removeFromParent();
         }
     }
 
@@ -83,16 +90,16 @@ public class CrowdDebugControl extends AbstractControl {
             timer = 0;
 
             if (visual) {
-                ColorRGBA col = stateToColor(agent);
-                setColor(col);
+                ColorRGBA color = stateToColor(agent);
+                setColor(color);
             }
 
             if (verbose) {
-                LOG.info("<========== BEGIN CrowdDebugControl [{}] index [{}] ==========>", spatial.getName(), agent.idx);
-                LOG.info("isActive              [{}]", agent.active);
-                LOG.info("MoveRequestState      [{}]", agent.targetState);
-                LOG.info("CrowdAgentState       [{}]", agent.state);
-                LOG.info("<========== END   CrowdDebugControl [{}] index [{}] ==========>", spatial.getName(), agent.idx);
+                logger.info("<========== BEGIN CrowdDebugControl [{}] index [{}] ==========>", spatial.getName(), agent.idx);
+                logger.info("isActive              [{}]", agent.active);
+                logger.info("MoveRequestState      [{}]", agent.targetState);
+                logger.info("CrowdAgentState       [{}]", agent.state);
+                logger.info("<========== END   CrowdDebugControl [{}] index [{}] ==========>", spatial.getName(), agent.idx);
             }
         }
     }
@@ -123,13 +130,12 @@ public class CrowdDebugControl extends AbstractControl {
     }
 
     @Override
-    protected void controlRender(RenderManager rm, ViewPort vp) {
-    }
+    protected void controlRender(RenderManager rm, ViewPort vp) {}
 
-    protected void setColor(ColorRGBA c) {
-        if (curColor != c) {
-            halo.getMaterial().setColor("Color", c);
-            curColor = c;
+    private void setColor(ColorRGBA color) {
+        if (curColor != color) {
+            wireMaterial.setColor("Color", color);
+            curColor = color;
         }
     }
 
@@ -148,7 +154,7 @@ public class CrowdDebugControl extends AbstractControl {
      * @param visual the visual to set.
      */
     public void setVisual(boolean visual) {
-        this.halo.setCullHint(visual ? CullHint.Inherit : CullHint.Always);
+        this.debugNode.setCullHint(visual ? CullHint.Inherit : CullHint.Always);
         this.visual = visual;
     }
 
@@ -168,6 +174,50 @@ public class CrowdDebugControl extends AbstractControl {
      */
     public void setVerbose(boolean verbose) {
         this.verbose = verbose;
+    }
+
+    private Node createDebugCylinder(float radius, float height) {
+
+        Node body = new Node("Cylinder");
+
+        for (int i = 0; i < 4; i++) {
+            float x = FastMath.sin(FastMath.HALF_PI * i) * radius;
+            float z = FastMath.cos(FastMath.HALF_PI * i) * radius;
+            Vector3f start = new Vector3f(x, 0, z);
+            Vector3f end = new Vector3f(x, height, z);
+
+            Geometry line = createLine(start, end);
+            line.setName("Line-" + i);
+            body.attachChild(line);
+        }
+
+        Geometry c1 = createCircle(16, radius);
+        c1.setName("Bottom.Circle");
+        c1.setLocalTranslation(0, 0, 0);
+        body.attachChild(c1);
+
+        Geometry c2 = createCircle(16, radius);
+        c1.setName("Top.Circle");
+        c2.setLocalTranslation(0, height, 0);
+        body.attachChild(c2);
+
+        return body;
+    }
+
+    private Geometry createCircle(int samples, float radius) {
+        Circle circle = new Circle(radius, samples);
+        Geometry geo = new Geometry("Circle", circle);
+        geo.setShadowMode(ShadowMode.Off);
+        geo.setMaterial(wireMaterial);
+        return geo;
+    }
+
+    private Geometry createLine(Vector3f start, Vector3f end) {
+        Line line = new Line(start, end);
+        Geometry geo = new Geometry("Line", line);
+        geo.setShadowMode(ShadowMode.Off);
+        geo.setMaterial(wireMaterial);
+        return geo;
     }
 
 }
