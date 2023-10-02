9563b1650b3bb7381b11b68d456fbcf3560009d8
capdevon <jefferydeaver2010@gmail.com>
2023-10-02 14:54:12 +0200

    new load/save methods

--- src/main/java/com/jme3/recast4j/editor/NavMeshProperties.java
+++ src/main/java/com/jme3/recast4j/editor/NavMeshProperties.java
@@ -1,57 +1,107 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jme3.recast4j.editor;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author capdevon
- */
 public class NavMeshProperties {
 
+    private static final Logger logger = Logger.getLogger(NavMeshProperties.class.getName());
+    
     private NavMeshProperties() {}
-
+    
     /**
-     *
+     * Export the NavMeshBuildSettings to a file.
+     * 
+     * @param settings
+     * @param file
+     */
+    public static void save(NavMeshBuildSettings settings, File file) {
+
+        logger.log(Level.INFO, "Saving File={0}", file.getAbsolutePath());
+        
+        try (OutputStream output = new FileOutputStream(file)) {
+            Properties prop = toProperties(settings);
+            String comments = "NavMeshBuildSettings properties file";
+            prop.store(output, comments);
+            
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, "Error: Failed to save NavMeshBuildSettings!", ex);
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    private static <T> Properties toProperties(T t) 
+            throws IntrospectionException, ReflectiveOperationException {
+
+        Class<T> c = (Class<T>) t.getClass();
+        BeanInfo beanInfo = Introspector.getBeanInfo(c, Object.class);
+        Properties prop = new Properties();
+
+        for (PropertyDescriptor pd : beanInfo.getPropertyDescriptors()) {
+            String name = pd.getName();
+            Object o = pd.getReadMethod().invoke(t);
+            if (o != null) {
+                prop.setProperty(name, o.toString());
+            }
+        }
+        
+        return prop;
+    }
+    
+    /**
+     * Reads a NavMeshBuildSettings from the input file.
+     * 
      * @param file
      * @return
      * @throws IOException
      */
-    public static NavMeshBuildSettings fromFile(File file) throws IOException {
-
-        System.out.println("--Loading File=" + file.getAbsolutePath());
-
-        try (FileInputStream fis = new FileInputStream(file)) {
-
-            Properties props = new Properties();
-            props.load(fis);
-
-            NavMeshBuildSettings settings = new NavMeshBuildSettings();
-            settings.tiled                  = getBoolean(props, "tiled");
-            settings.tileSize               = getInteger(props, "tileSize");
-            settings.cellSize               = getFloat(props, "cellSize");
-            settings.cellHeight             = getFloat(props, "cellHeight");
-            settings.agentHeight            = getFloat(props, "agentHeight");
-            settings.agentRadius            = getFloat(props, "agentRadius");
-            settings.agentMaxClimb          = getFloat(props, "agentMaxClimb");
-            settings.agentMaxSlope          = getFloat(props, "agentMaxSlope");
-            settings.regionMinSize          = getInteger(props, "regionMinSize");
-            settings.regionMergeSize        = getInteger(props, "regionMergeSize");
-            settings.edgeMaxLen             = getFloat(props, "edgeMaxLen");
-            settings.edgeMaxError           = getFloat(props, "edgeMaxError");
-            settings.vertsPerPoly           = getInteger(props, "vertsPerPoly");
-            settings.detailSampleDist       = getFloat(props, "detailSampleDist");
-            settings.detailSampleMaxError   = getFloat(props, "detailSampleMaxError");
-
-            return settings;
+    public static NavMeshBuildSettings load(File file) throws IOException {
+        try (FileInputStream input = new FileInputStream(file)) {
+            return load(input);
         }
+    }
+    
+    /**
+     * Reads a NavMeshBuildSettings from the input byte stream.
+     * 
+     * @param input
+     * @return
+     * @throws IOException
+     */
+    public static NavMeshBuildSettings load(InputStream input) throws IOException {
+        
+        Properties props = new Properties();
+        props.load(input);
+
+        NavMeshBuildSettings settings = new NavMeshBuildSettings();
+        settings.tiled                  = getBoolean(props, "tiled");
+        settings.tileSize               = getInteger(props, "tileSize");
+        settings.cellSize               = getFloat(props, "cellSize");
+        settings.cellHeight             = getFloat(props, "cellHeight");
+        settings.agentHeight            = getFloat(props, "agentHeight");
+        settings.agentRadius            = getFloat(props, "agentRadius");
+        settings.agentMaxClimb          = getFloat(props, "agentMaxClimb");
+        settings.agentMaxSlope          = getFloat(props, "agentMaxSlope");
+        settings.regionMinSize          = getInteger(props, "regionMinSize");
+        settings.regionMergeSize        = getInteger(props, "regionMergeSize");
+        settings.edgeMaxLen             = getFloat(props, "edgeMaxLen");
+        settings.edgeMaxError           = getFloat(props, "edgeMaxError");
+        settings.vertsPerPoly           = getInteger(props, "vertsPerPoly");
+        settings.detailSampleDist       = getFloat(props, "detailSampleDist");
+        settings.detailSampleMaxError   = getFloat(props, "detailSampleMaxError");
+
+        return settings;
     }
 
     private static float getFloat(Properties props, String key) {
