db7b6f75833e30fe7e3b2f8969e289c26a1a1c6a
capdevon <jefferydeaver2010@gmail.com>
2023-10-02 14:54:27 +0200

    fix

--- src/main/java/com/jme3/recast4j/demo/utils/GameObject.java
+++ src/main/java/com/jme3/recast4j/demo/utils/GameObject.java
@@ -1,6 +1,10 @@
 package com.jme3.recast4j.demo.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.jme3.scene.Node;
+import com.jme3.scene.SceneGraphVisitor;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.control.Control;
 
@@ -18,24 +22,50 @@ public class GameObject {
     /**
      * Returns the component of Type type if the game object has one attached,
      * null if it doesn't.
-     *
-     * @param <T>
-     * @param spatial
-     * @param clazz
-     * @return
      */
-    public static <T extends Control> T getComponent(Spatial spatial, Class<T> clazz) {
-        return spatial.getControl(clazz);
+    public static <T extends Control> T getComponent(Spatial sp, Class<T> clazz) {
+       return sp.getControl(clazz);
+    }
+    
+    /**
+     * Returns all components of Type type in the GameObject.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends Control> List<T> getComponents(Spatial sp, Class<T> type) {
+        List<T> lst = new ArrayList<>(3);
+        for (int i = 0; i < sp.getNumControls(); i++) {
+            T control = (T) sp.getControl(i);
+            if (type.isAssignableFrom(control.getClass())) {
+                lst.add(control);
+            }
+        }
+        return lst;
+    }
+    
+    /**
+     * Returns all components of Type type in the GameObject or any of its
+     * children using depth first search. Works recursively.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends Control> List<T> getComponentsInChildren(Spatial subtree, Class<T> type) {
+        List<T> lst = new ArrayList<>(3);
+        subtree.depthFirstTraversal(new SceneGraphVisitor() {
+            @Override
+            public void visit(Spatial sp) {
+                for (int i = 0; i < sp.getNumControls(); i++) {
+                    T control = (T) sp.getControl(i);
+                    if (type.isAssignableFrom(control.getClass())) {
+                        lst.add(control);
+                    }
+                }
+            }
+        });
+        return lst;
     }
 
     /**
      * Returns the component of Type type in the GameObject or any of its
      * children using depth first search.
-     *
-     * @param <T>
-     * @param spatial
-     * @param clazz
-     * @return
      */
     public static <T extends Control> T getComponentInChildren(Spatial spatial, final Class<T> clazz) {
         T control = spatial.getControl(clazz);
@@ -58,11 +88,6 @@ public class GameObject {
     /**
      * Retrieves the component of Type type in the GameObject or any of its
      * parents.
-     *
-     * @param <T>
-     * @param spatial
-     * @param clazz
-     * @return
      */
     public static <T extends Control> T getComponentInParent(Spatial spatial, Class<T> clazz) {
         Node parent = spatial.getParent();
